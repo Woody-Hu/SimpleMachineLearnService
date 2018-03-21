4b3146afd58c87bcd77ea0490598efbf18742215
Woody-Hu <812791840@qq.com>
2018-03-21 17:31:22 +0800

    2018 03 21

--- LearningModelService.py
+++ LearningModelService.py
@@ -71,7 +71,26 @@ class LearningModelService:
     def insert_evaluate_value(self,input_name,input_x_values,input_y_values):
         self._insert_model_result_value(input_name + _use_name_suffix_evaluate_value,input_x_values,input_y_values)
     
+    @model_name_check(True)
+    def get_train_value(self,input_name,input_limit,input_skip):
+        return self._get_model_result_value(input_name + _use_name_suffix_train_value,input_limit,input_skip)
     
+    @model_name_check(True)
+    def get_evaluate_value(self,input_name,input_limit,input_skip):
+        return self._get_model_result_value(input_name + _use_name_suffix_evaluate_value,input_limit,input_skip)
+    
+    def _get_model_result_value(self,input_collection_name,input_limit,input_skip):
+        self._useDb.change_collection(input_collection_name)   
+        findValues = self._useDb.find_all(input_limit, input_limit)
+        self._useDb.change_collection(_use_name_collection)
+        
+        x_value =[]
+        y_value =[]
+        
+        for oneValue in findValues:
+            x_value.append(oneValue[_str_x_value])
+            y_value.append(oneValue[_str_y_value])
+        return (x_value,y_value)
     
     def _insert_model_result_value(self,input_collection_name,input_x_values,input_y_values):
         if len(input_x_values) != len(input_y_values):
